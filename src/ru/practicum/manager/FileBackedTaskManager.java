38d5facfaff1a1940d84c6d51201573a42101b49
Антон Лучик <intemic@yandex.ru>
2025-03-19 16:28:03 +0500

    Created class exception

--- src/ru/practicum/manager/FileBackedTaskManager.java
+++ src/ru/practicum/manager/FileBackedTaskManager.java
@@ -1,5 +1,9 @@
 package ru.practicum.manager;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
+    private String filename;
 
+    public FileBackedTaskManager(final String filename) {
+        this.filename = filename;
+    }
 }
